7cf52749549e153823caf406e25908f22e6b5259
Joe Baranski <joe.baranski49@gmail.com>
2020-11-13 17:39:11 -0500

    working on products page

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,7 +1,10 @@
 export default function Products() {
   return (
     <div>
-      
+      <h2>All our great products!</h2>
+      <ul>
+        <li>$79.95 - Mares X-VU Liquidskin Scuba Diving Mask</li>
+      </ul>
     </div>
   )
 }
